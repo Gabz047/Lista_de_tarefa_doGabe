44c169166df9c30ca7f5fba129b953bdbd6b6f35
Gabz047 <gabe1612mail@gmail.com>
2023-12-06 09:41:40 -0300

    AAAAAAA

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let standBy = document.getElementById("lista-tarefa");
 let nameTask = document.getElementById('addTarefa');
 
-
 function addTask() {
-   
-    if (nameTask.value == "") {
-        alert("Escreva algo antes de adicionar");
-    } else {
-       standBy.innerHTML += `<li class="tarefa-box">${nameTask.value} <button id="btn-finalizar" onclick="finalizar()">Finalizar</button> <button id="btn-editar" onclick="editar()">Editar</button></li>`;
-       nameTask.value = "";
-    }
+    let tarefaBox = document.createElement("li");
+    tarefaBox.classList.add("tarefaBox");
+    let txt = document.createElement("p");
+    txt.innerHTML = nameTask.value;
+    tarefaBox.appendChild(txt);
+    let btnExcluir = document.createElement("button");
+    btnExcluir.classList.add("btnEx-stand");
+    btnExcluir.innerText = "Excluir"
+    tarefaBox.appendChild(btnExcluir);
+    document.getElementById("lista-tarefa").appendChild(tarefaBox);
 }
 
-function finalizar() {
-   let finalizada = standBy.innerHTML = `<li class="tarefa-box">${nome.value} <button id="btn-excluir" onclick="excluir()">Excluir</button> <button id="btn-retornar" onclick="retornar()">Retornar</button></li>`;
-   standBy.innerHTML = "";
-    let listaF = document.getElementById("lista-final");
-    listaF.innerHTML = finalizada;
-}
+    let btnExcluir = document.getElementsByClassName("btnEx-stand");
+    btnExcluir.addEventListener("click", function() {
+        standBy.innerHTML = "";
+    });
